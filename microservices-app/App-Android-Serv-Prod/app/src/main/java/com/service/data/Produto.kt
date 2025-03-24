6d853d4042cb7e37a5b3ef4db1395cd3ab612e5a
Sr. Lake <84886995+etmaduckso@users.noreply.github.com>
2025-03-24 10:45:59 -0300

    AT-0.0.1 KTL/PY

--- microservices-app/App-Android-Serv-Prod/app/src/main/java/com/service/data/Produto.kt
+++
@@ -1,7 +0,0 @@
-package com.service.data
-
-data class Produto(
-    val nome: String,
-    val descricao: String,
-    val preco: Double
-)
--- microservices-app/App-Android-Serv-Prod/app/src/main/java/com/service/data/Servico.kt
+++
@@ -1,7 +0,0 @@
-package com.service.data
-
-data class Servico(
-    val nome: String,
-    val descricao: String,
-    val preco: Double
-)
